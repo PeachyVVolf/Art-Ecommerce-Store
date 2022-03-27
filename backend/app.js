bb0e32aae2657ce5163656fcd41c87d72c5dbb84
Ahmad Ajmal <ahmadajmal9301@gmail.com>
2022-03-28 00:24:02 +0500

    Created Order Model and Create, Get Order Methods

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const cookieParser = require("cookie-parser");
 const express = require("express");
 const app = express();
-const errorMiddleware = require("./middleware/error");
+const errorMiddleware = require("./middleware/error"); 
 
 app.use(cookieParser());
 app.use(express.json());
@@ -9,9 +9,11 @@ app.use(express.json());
 // Route Imports
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
 
 app.use("/api/v1", product);
 app.use("/api/v1", user);
+app.use("/api/v1", order);
 
 //MiddleWare for Error
 app.use(errorMiddleware);
